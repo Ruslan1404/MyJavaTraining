04db2115cdf4c0481e905ae5d7b7851c19f06a82
Ruslan1404 <rturyatko@gmail.com>
2023-03-09 17:49:31 +0100

    add file

--- src/lesson15/Main.java
+++ src/lesson15/Main.java
@@ -1,8 +1,27 @@
 package lesson15;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        /*System.out.println("Введите втроку");
+        String string = input.nextLine();
+        System.out.println(string);
 
-        System.out.println("Hello");
+        String string = input.next();
+        System.out.println(string);
+        String string2 = input.next();
+        System.out.println(string2);
+        String string3 = input.next();
+        System.out.println(string3);*/
+
+        /*System.out.printf("После знака %%n%nвсе должно начинаться%nс новой строки%n");
+
+        System.out.printf("просто текст %b просто текст", 6);*/
+
+        System.out.printf("В кавычках \"%2$s\" должен быть текст. Тут тоже: %1$s", "String", "String2");
+
+
     }
 }
