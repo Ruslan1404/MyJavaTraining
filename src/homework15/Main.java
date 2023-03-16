1822bddcc83db3dde770ecada87d9e98df81260c
Ruslan1404 <rturyatko@gmail.com>
2023-03-16 14:06:49 +0100

    add file

--- src/homework15/Main.java
+++ src/homework15/Main.java
@@ -37,13 +37,58 @@ public class Main {
                 "можно построить треугольник" +
                 "и это - \"" + isTriangle+"\""+".");*/
 
+        //Задание 3//
+        /*System.out.println("Введите свой год рождения");
+        //int year = input.nextInt();
+        //System.out.println(year);
+        //String year = input.nextLine();
+        int age = 2023-Integer.parseInt(input.nextLine());
+        System.out.println(age);*/
+
 
         //Задание 5//
-        /*Date date = new Date();
+        /*System.out.println("Введите спецификатор формата для нынешнего времени."+
+                "В качестве разделителя использовать пробел");
+        String specFormat = input.next();
+        Date date = new Date();
+        String date2 = String.format(specFormat, date);
+        System.out.println(date2);*/
+
+
+        //Задание 6//
+        System.out.println("Уважаемый X, Вы выполняете задание номер Y," +
+                " и делаете это в Z часов W минут в V.");
+        System.out.println("Введите своё имя и через пробел номер задания");
+        String name = input.next();
+        int taskNumber = input.nextInt();
+        System.out.println("Введите спицификаторы формата вместо X, Y, Z, W, V.\n" +
+                "где X - это имя пользователя,\n" +
+                "Y - порядковый номер ДЗ,\n" +
+                "Z - время в часах в момент выполнения программы,\n" +
+                "W - время в минутах в момент выполнения программы,\n" +
+                "V - день недели. Все спецификаторы форматов вводить через пробел.");
+        Scanner input1 = new Scanner(System.in);
+        String string = input1.nextLine();
+        System.out.println(string);
+        String string1 = "Уважаемый X, Вы выполняете задание номер Y," +
+                " и делаете это в Z часов W минут в V.";
+        String string2 = string.replace("X", string.split(" ")[0]).
+                replace("Y", string.split(" ")[1]).
+                replace("Z", string.split(" ")[2]).
+                replace("W", string.split(" ")[3]).
+                replace("V", string.split(" ")[4]);
+        System.out.println(string2);
+        Date date = new Date();
         System.out.println(date);
-        System.out.printf("%tT\n", date);
-        System.out.printf("%1$tH:%1$tM:%1$tS:%1$tL:%1$tN %1$tp", date);
-        System.out.printf("%1$td:%1$tm:%1$ty\n", date);*/
+        String string3 = String.format(string2, name, taskNumber, date, date, date);
+        System.out.println(string3);
+
+
+
+
+
+
+
 
     }
 }
