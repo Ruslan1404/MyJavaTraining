0ac60daeca0057f15d10b73de92339ba7478fe60
Ruslan1404 <rturyatko@gmail.com>
2023-03-16 11:45:50 +0100

    add file

--- src/lesson15/Main.java
+++ src/lesson15/Main.java
@@ -19,6 +19,24 @@ public class Main {
         String string3 = input.next();
         System.out.println(string3);*/
 
+         /*Scanner in = new Scanner(System.in);
+        System.out.println("Введите строку");
+        String string = in.nextLine();
+        int count = string.split(" ").length;
+        if(count>12&&count<20&&count%3!=0) {
+            System.out.println("блок 1");
+        }else if (count>3&&count<9&&count%2==0) {
+            System.out.println("блок2");
+        }else if (count==3){
+            System.out.println("блок3");
+        }else{
+            System.out.println("ничего");*/
+
+        /*System.out.println("введите возраст");
+        int age = in.nextInt();
+        String message = age<18?"Вы несовершеннолетний гражданин":"Вы совершеннолетний гражданин";
+        System.out.println(message);*/
+
 
         //Formatter форматирование в виде след метода//
         //sys.out.printf(format,arguments)//
--- src/lesson16/Main.java
+++ src/lesson16/Main.java
@@ -4,23 +4,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        /*System.out.println("Введите строку");
-        String string = in.nextLine();
-        int count = string.split(" ").length;
-        if(count>12&&count<20&&count%3!=0) {
-            System.out.println("блок 1");
-        }else if (count>3&&count<9&&count%2==0) {
-            System.out.println("блок2");
-        }else if (count==3){
-            System.out.println("блок3");
-        }else{
-            System.out.println("ничего");*/
+       /*   if(условие){
+          (этот блок выполнится
+          если условие = true);
+    } else {
+          (этот блок выполнится
+          если условие = false);
+          }*/
 
-        System.out.println("введите возраст");
+        int x = 15;
+        if(x<10){
+            System.out.println("Блок if");
+        } else if (x>20) {
+            System.out.println("Блок else if");
+        } else
+         {
+            System.out.println("Блок else");
+        }
+
+
+
+        //Задана стоимость билета по маршруту Днепр - Киев.
+        //В зависимости от возраста предоставляется скидка
+        /*double cost = 500;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Age");
         int age = in.nextInt();
-        String message = age<18?"Вы несовершеннолетний гражданин":"Вы совершеннолетний гражданин";
-        System.out.println(message);
+        if(age<18){
+            if(age==13){
+                cost = cost*0.5;
+                System.out.println("цена "+ cost);
+            }
+            cost = cost*0.75;
+            System.out.println("Цена билета "+ cost);
+        } else if (age>55) {
+            cost = cost*0.85;
+            System.out.println("Цена билета "+ cost);
+        }
+        else  {
+            System.out.println("Цена билета "+ cost);
+        }*/
 
 
     }
